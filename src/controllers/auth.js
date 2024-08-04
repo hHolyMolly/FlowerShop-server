172b1c9be7b2bb28577b732e322fa15b19d5f458
HolyMolly <developer.code5417@gmail.com>
2024-08-04 16:16:08 +0300

    Fix UserModel

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-import UserModel from "../models/User";
+import UserModel from "../models/User.js";
 
 export const register = async (req, res) => {
   try {
--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  roles: [{ type: String, ref: "Role", default: ["USER"] }],
-});
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    roles: [{ type: String, ref: "Role", default: ["USER"] }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("User", UserSchema);
